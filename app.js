fd225dc2c3465446d584edf191f9a84f58bf3565
David Lara <celfcool@gmail.com>
2023-07-01 14:56:11 -0300

    html + swagger updates

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
 // extra security packages
-const helmet = require('helmet');
-const cors = require('cors');
-const xss = require('xss-clean');
-const rateLimiter = require('express-rate-limit');
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 
 // Swagger
-const swaggerUI = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const connectDB = require('./db/connect');
-const authenticateUser = require('./middleware/authentication');
+const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
 // routers
-const authRouter = require('./routes/auth');
-const jobsRouter = require('./routes/jobs');
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
@@ -36,14 +36,54 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-app.get('/', (req, res) => {
-  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+app.get("/", (req, res) => {
+  const html = `
+    <html>
+      <head>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            text-align: center;
+          }
+  
+          h1 {
+            color: #333;
+          }
+  
+          .container {
+            margin-top: 100px;
+          }
+  
+          .link {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+          }
+  
+          .link:hover {
+            background-color: #45a049;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Jobs API</h1>
+          <a class="link" href="/api-docs">Documentation</a>
+        </div>
+      </body>
+    </html>
+  `;
+  res.send(html);
 });
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
